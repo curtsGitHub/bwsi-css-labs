3b2e33b56df6d52d609a0dba882c57b27d59f45b
curtsGitHub <144584761+curtsGitHub@users.noreply.github.com>
2026-02-18 23:36:16 -0800

    try again

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -6,7 +6,6 @@ then performs the operation and returns the result.
 
 The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
-committttt
 
 """
 
